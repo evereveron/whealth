febe9ec38a70194316a574a41c5fc8cafb89501d
Jasmine Feng <feng.jasmine54@gmail.com>
2016-02-20 17:14:44 -0500

    trump and nav

---
+++ Assets/Scripts/TrashMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Navigate to Castle
+		GameObject player = GameObject.Find("Player");
+		if (player)
+			GetComponent<NavMeshAgent>().destination = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
